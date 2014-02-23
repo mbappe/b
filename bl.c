d3ed1d049741f27713fe84a2eaedb0abf2521a6c
Mike Bappe <michaelbappe@gmail.com>
2014-02-22 19:52:48 -0700

    Now it generates code.

--- bl.c
+++ bl.c
@@ -1,9 +1,15 @@
 
 // This file is a wrapper for bli.c.
-// It exists so we can generate .o, .s and .i files which include only Lookup.
+// It exists so we can generate bl.o, bl.s and bl.i files which include
+// only Lookup.
+// Unfortunately, the compiler complains that Lookup isn't used and doesn't
+// generate any code.  Why?  
+// So we have __p to work around it.
 
 #include "b.h"
 
 #define LOOKUP
 #include "bli.c"
+
+void *__p = Lookup;
 
