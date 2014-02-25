2a89ea6fd9fcf723a25d016d1b0117ec105213f4
Mike Bappe <michaelbappe@gmail.com>
2014-02-25 11:13:37 -0700

    remove

---
+++ br.c
@@ -0,0 +1,15 @@
+
+// This file is a wrapper for bli.c.
+// It exists so we can generate br.o, br.s and br.i files which include
+// only Remove.
+// Unfortunately, the compiler complains that Remove isn't used and doesn't
+// generate any code.  Why?  
+// So we have __pRemove to work around it.
+
+#include "b.h"
+
+#define REMOVE
+#include "bli.c"
+
+void *__pRemove = (void *)Remove;
+
