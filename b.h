4386879e3c7cd1c5ba65e38b301d9e41f7686d14
Mike Bappe <michaelbappe@gmail.com>
2015-01-21 13:07:00 -0700

    fast defaults

--- b.h
+++ b.h
@@ -1,5 +1,5 @@
 
-// @(#) $Id: b.h,v 1.374 2015/01/21 05:22:19 mike Exp $
+// @(#) $Id: b.h,v 1.375 2015/01/21 15:04:49 mike Exp $
 // @(#) $Source: /Users/mike/b/RCS/b.h,v $
 
 #if ( ! defined(_B_H_INCLUDED) )
@@ -356,10 +356,10 @@ typedef Word_t Bucket_t;
   #define LVL_IN_WR_HB
 #endif // ! defined(DEPTH_IN_SW) && ! defined(NO_LVL_IN_WR_HB)
 
-// Default is -DSKIP_TO_XX_SW.
-#if ! defined(NO_SKIP_TO_XX_SW)
-#define SKIP_TO_XX_SW
-#endif // ! defined(NO_SKIP_TO_XX_SW)
+// Default is -USKIP_TO_XX_SW.
+//#if ! defined(NO_SKIP_TO_XX_SW)
+//#define SKIP_TO_XX_SW
+//#endif // ! defined(NO_SKIP_TO_XX_SW)
 
 // Default is -DSKIP_TO_BITMAP if -DSKIP_TO_XX_SW.
 #if defined(SKIP_TO_XX_SW)
@@ -1066,12 +1066,21 @@ EmbeddedListPopCntMax(int nBL)
 // Can we use lvl to id skip instead of a bit in the type field?
 // XxSwWidth is the width of the switch.
 // ListPopCnt is the number of keys in the list.
+// A field at the end is faster to extract than a field in the middle.
 #define cnBitsLvl  7
 #define cnLsbLvl  (cnBitsPerWord - cnBitsLvl)
 #define cnBitsXxSwWidth   6
+#if defined(SKIP_TO_XX_SW)
 #define cnLsbXxSwWidth  cnBitsVirtAddr
+#else // defined(SKIP_TO_XX_SW)
+#define cnLsbXxSwWidth  (cnBitsPerWord - cnBitsXxSwWidth)
+#endif // defined(SKIP_TO_XX_SW)
 #define cnBitsListPopCnt  9
-#define cnLsbListPopCnt cnBitsVirtAddr
+#if defined(SKIP_TO_LIST)
+#define cnLsbListPopCnt  cnBitsVirtAddr
+#else // defined(SKIP_TO_LIST)
+#define cnLsbListPopCnt  (cnBitsPerWord - cnBitsListPopCnt)
+#endif // defined(SKIP_TO_LIST)
 
 #if defined(CODE_XX_SW)
 
