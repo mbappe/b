ab0e166b2f4b53df8ca1e6742be38ca113c08ad2
Mike Bappe <michaelbappe@gmail.com>
2016-12-17 10:21:16 -0700

    Get rid of incorrect comments

--- bi.c
+++ bi.c
@@ -2,9 +2,6 @@
 // This file is a wrapper for bli.c.
 // It exists so we can generate bi.o, bi.s and bi.i files which include
 // only Insert.
-// Unfortunately, the compiler complains that Insert isn't used and doesn't
-// generate any code.  Why?  
-// So we have __pInsert to work around it.
 
 #define INSERT
 #include "b.h"
--- bl.c
+++ bl.c
@@ -2,9 +2,6 @@
 // This file is a wrapper for bli.c.
 // It exists so we can generate bl.o, bl.s and bl.i files which include
 // only Lookup.
-// Unfortunately, the compiler complains that Lookup isn't used and doesn't
-// generate any code.  Why?  
-// So we have __pLookup to work around it.
 
 #define LOOKUP
 #include "b.h"
