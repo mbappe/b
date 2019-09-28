771d314b59d8d36f9965403767a554d9e01dabc1
Mike Bappe <michaelbappe@gmail.com>
2019-09-28 06:27:10 -0600

    Simplify bdefines.h undefs for Judy1.

--- bdefines.h
+++ bdefines.h
@@ -493,12 +493,8 @@
 #endif // B_JUDYL
 
 #ifndef B_JUDYL
-  #ifdef BMLF_CNTS
-    #undef BMLF_CNTS
-  #endif // BMLF_CNTS
-  #ifdef PREFETCH_BMLF_CNTS
-    #undef PREFETCH_BMLF_CNTS
-  #endif // PREFETCH_BMLF_CNTS
+  #undef BMLF_CNTS
+  #undef PREFETCH_BMLF_CNTS
 #endif // B_JUDYL
 
 #endif // ( ! defined(_BDEFINES_H_INCLUDED) )
